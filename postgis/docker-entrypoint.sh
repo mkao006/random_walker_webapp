00eb921a7868edf31d757e641565cbcd3d20ebd2
mk <mkao006@gmail.com>
2016-07-21 21:47:29 -0700

    moved configuration from Dockerfile to docker-entrypoint.sh

--- postgis/docker-entrypoint.sh
+++ postgis/docker-entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+psql -U postgres -c "CREATE USER root WITH SUPERUSER PASSWORD 'password';"
+createdb -O root LocationDB
+psql -U postgres -d LocationDB -c "CREATE EXTENSION postgis";
+
+echo "host all  all    0.0.0.0/0  md5" >> /etc/postgresql/9.3/main/pg_hba.conf
+echo "listen_addresses='*'" >> /etc/postgresql/9.3/main/postgresql.conf
+
+
 echo Starting postgres
 exec start-stop-daemon --start --chuid postgres:postgres \
      --exec /usr/lib/postgresql/9.3/bin/postgres -- \
