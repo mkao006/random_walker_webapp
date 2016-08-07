deea5403340ebcf65a5357ba731b1f4d4e8caad6
mk <mkao006@gmail.com>
2016-08-06 17:34:31 -0700

    added case for arguements

--- deploy_random_walker.sh
+++ deploy_random_walker.sh
@@ -12,17 +12,31 @@ appName="random_walker"
 awsConfigDir="$rootDir/config/aws/"
 export envName="random-walker"
 
-read -p "Deploy type [local/aws]: "\
-     deploy_type
-echo "Deploying $deploy_type"
 
+if [ "$#" -eq 0 ]
+then
+
+    read -p "Deploy type [local/eb]: "\
+         deploy_type
+    echo "Deploying $deploy_type"
+else
+    for arg in "$@"
+    do
+        case $arg in
+            "--local" )
+                deploy_type="local";;
+            "--eb" )
+                deploy_type="eb";;
+        esac
+    done
+fi
 
 if [ "$deploy_type" = "local" ]
 then
     docker-compose stop
     docker-compose build
     docker-compose up -d
-elif [ "$deploy_type" = "aws" ]
+elif [ "$deploy_type" = "eb" ]
 then
     eb create $envName -i t2.micro --single
     eb console
