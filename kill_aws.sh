cfcca8585e1dd517f2bbdc6a2bf8d516e4ed5ef6
mk <mkao006@gmail.com>
2016-08-25 17:21:23 -0700

    removed redudant local deployment

--- kill_aws.sh
+++ kill_aws.sh
@@ -4,20 +4,5 @@
 appName="random_walker"
 
 
-if [ "$deploy_type" = "local" ]
-then
-    docker-compose stop
-    docker-compose rm -f
-elif [ "$deploy_type" = "eb" ]
-then
-    eb terminate $envName
-elif [ "$deploy_type" = "all" ]
-then
-    docker-compose stop
-    docker-compose rm -f
-    eb terminate $envName
-else
-    echo "Inappropriate deployment type, must be 'local', 'eb' or 'all'"
-fi
-
+eb terminate $envName
 unset envName
